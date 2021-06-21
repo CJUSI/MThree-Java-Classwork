abab866eaf75c0199404582e39de642ecc9508bc
CJUSI <69305896+CJUSI@users.noreply.github.com>
2021-06-21 09:27:39 +0100

    Update bookdb_schema.sql

--- SQL/EngageActivities/bookdb_schema.sql
+++ SQL/EngageActivities/bookdb_schema.sql
@@ -7,69 +7,69 @@ USE booksdb;
 
 -- Create tables shown in ERD --
 CREATE TABLE Author (
-	AuthorID 		INT				AUTO_INCREMENT		NOT NULL,
+    AuthorID 		INT			AUTO_INCREMENT		NOT NULL,
     CONSTRAINT pk_Author
 		PRIMARY KEY(AuthorID),
     FirstName		varchar(25)		NOT NULL,
     MiddleName		varchar(25),
-    LastName		varchar(25) 	NOT NULL,
-    Gender			varchar(1),
-    DoB				DATE			NOT NULL,
-    DoD				DATE
+    LastName		varchar(25) 		NOT NULL,
+    Gender		varchar(1),
+    DoB			DATE			NOT NULL,
+    DoD			DATE
 );
 
 CREATE TABLE Book (
-	BookID			INT				AUTO_INCREMENT		NOT NULL,
+    BookID		INT			AUTO_INCREMENT		NOT NULL,
     CONSTRAINT pk_Book
 		PRIMARY KEY (BookID),
-	Title			varchar(100) 	NOT NULL,
-    PublicationDate DATE
+    Title		varchar(100) 		NOT NULL,
+    PublicationDate 	DATE
 );
 
 CREATE TABLE Format (
-	FormatID		INT				AUTO_INCREMENT		NOT NULL,
+    FormatID		INT			AUTO_INCREMENT		NOT NULL,
     CONSTRAINT pk_Format
 		PRIMARY KEY(FormatID),
     FormatName		varchar(12)		NOT NULL
 );
 
 CREATE TABLE Genre (
-	GenreID			INT 			AUTO_INCREMENT		NOT NULL,
+    GenreID		INT 			AUTO_INCREMENT		NOT NULL,
     CONSTRAINT pk_Genre
 		PRIMARY KEY(GenreID),
-	GenreName		varchar(25)		NOT NULL
+    GenreName			varchar(25)	NOT NULL
 );
 
 CREATE TABLE BookAuthor (
-	AuthorID		INT				NOT NULL,
+    AuthorID		INT			NOT NULL,
     CONSTRAINT fk_AuthorBookID
 		FOREIGN KEY (AuthorID)
 		REFERENCES Author(AuthorID),
-	BookID			INT				NOT NULL,
+    BookID		INT			NOT NULL,
     CONSTRAINT fk_BookAuthorID
 		FOREIGN KEY (BookID)
 		REFERENCES Book(BookID)
 );
 
 CREATE TABLE BookFormat (
-	BookID			INT				NOT NULL,
+    BookID		INT			NOT NULL,
     CONSTRAINT fk_BookFormatID
 		FOREIGN KEY (BookID)
 		REFERENCES Book(BookID),
-	FormatID		INT				NOT NULL,
+    FormatID		INT			NOT NULL,
     CONSTRAINT fk_FormatBookID
 		FOREIGN KEY (FormatID)
 		REFERENCES Format(FormatID),
-	Price			DECIMAL,
+    Price		DECIMAL,
     QuantityOnHand	INT
 );
 
 CREATE TABLE BookGenre (
-	BookID			INT				NOT NULL,
+    BookID		INT			NOT NULL,
     CONSTRAINT fk_BookGenreID
 		FOREIGN KEY (BookID)
 		REFERENCES Book(BookID),
-	GenreID			INT 			NOT NULL,
+    GenreID		INT 			NOT NULL,
     CONSTRAINT fk_GenreBookID
 		FOREIGN KEY (GenreID)
 		REFERENCES Genre(GenreID)
